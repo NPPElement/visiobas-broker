ff7dde598fa5b3daefcaace7f0efbd361b6f0962
John <e.k@mail.ru>
2021-01-20 22:03:23 +0300

    Update init.sh

--- conf/mysql/init-files/init.sh
+++ conf/mysql/init-files/init.sh
@@ -8,8 +8,9 @@ mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD < /opt/init.sql
 sleep 5
 mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD < /opt/insert.sql
 
+sleep 5
 # Устанавливаем пароли
 mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD 
-update `auth`.`user` set `password`=md5('$VISIODESK_ADMIN_PASSWORD') where `login`='admin';
-update `vdesk`.`user` set `pass_hash`=md5('$VISIODESK_ADMIN_PASSWORD') where `login`='admin';
+update `auth`.`user` set `password`=md5($VISIODESK_ADMIN_PASSWORD) where `login`='admin';
+update `vdesk`.`user` set `pass_hash`=md5($VISIODESK_ADMIN_PASSWORD) where `login`='admin';
 exit;
