7a4b3499d9945d59247c65f928fc4e50031b83a2
John <e.k@mail.ru>
2021-01-20 17:52:02 +0300

    Update install.sh

--- run/install.sh
+++ run/install.sh
@@ -16,7 +16,10 @@ sudo apt --assume-yes install git jq
 VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
 DESTINATION=/usr/bin/docker-compose
 
-sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+
+#sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+
 
 sudo chmod 755 $DESTINATION
 docker-compose --version
