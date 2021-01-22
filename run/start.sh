eabc49ff142b8c932006b2d64037bd8296ccc3c8
John <e.k@mail.ru>
2021-01-22 20:02:48 +0300

    Update start.sh

--- run/start.sh
+++ run/start.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 export COMPOSE_HTTP_TIMEOUT=200
 
+spinner()
+{
+    local pid=$1
+    local delay=0.75
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
 # Это только для отладки
 cd /opt/services
 sudo docker-compose down
@@ -13,13 +28,7 @@ cd /opt/services
 #sudo docker-compose build --no-cache
 sudo docker-compose up -d --force-recreate
 
-chars="/-\|*"
-while :; do
-  for (( i=0; i<${#chars}; i++ )); do
-    sleep 0.5
-    echo -en "${chars:$i:1}" "\r"
-  done
-done
+spinner 30
 
 # Visiodesk запущен
 echo ' '
