499ea3951169fbbfc020823a5c49bc96679b9b11
John <e.k@mail.ru>
2021-01-22 20:16:41 +0300

    Update start.sh

--- run/start.sh
+++ run/start.sh
@@ -28,7 +28,7 @@ cd /opt/services
 #sudo docker-compose build --no-cache
 sudo docker-compose up -d --force-recreate
 
-(sleep 10) & spinner $!
+esho 'visiodesk запускается ' (sleep 60) & spinner $!
 
 # Visiodesk запущен
 echo ' '
