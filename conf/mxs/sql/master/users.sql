09ab2f7bd63ee30ec0e835147e4bd995eab6762b
John <e.k@mail.ru>
2021-01-23 17:09:14 +0300

    config

--- conf/mxs/sql/master/users.sql
+++ conf/mxs/sql/master/users.sql
@@ -7,3 +7,9 @@ CREATE USER 'maxuser'@'127.0.0.1' IDENTIFIED BY 'maxpwd';
 CREATE USER 'maxuser'@'%' IDENTIFIED BY 'maxpwd';
 GRANT ALL ON *.* TO 'maxuser'@'127.0.0.1' WITH GRANT OPTION;
 GRANT ALL ON *.* TO 'maxuser'@'%' WITH GRANT OPTION;
+
+
+-- создаем пользователя для доступа к базе данных с visiodesk 
+-- и разрешаем доступ с любого хоста в сети 172.16.16.0/24
+CREATE USER 'visiodesk'@'172.16.16.%' IDENTIFIED BY 'mkfkSbfTl';
+GRANT ALL PRIVILEGES ON *.* TO visiodesk@'172.16.16.%';
