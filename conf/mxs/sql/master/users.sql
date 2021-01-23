7019aed54d84c35f3d61ebf35b8ce4368e36b26a
John <e.k@mail.ru>
2021-01-23 13:14:07 +0300

    max

--- conf/mxs/sql/master/users.sql
+++ conf/mxs/sql/master/users.sql
@@ -3,9 +3,9 @@ RESET MASTER;
 SET GLOBAL max_connections=1000;
 SET GLOBAL gtid_strict_mode=ON;
 
-CREATE USER 'maxuser'@'127.0.0.1' IDENTIFIED BY 'maxpwd';
+CREATE USER 'maxuser'@'172.16.16.65' IDENTIFIED BY 'maxpwd';
 CREATE USER 'maxuser'@'%' IDENTIFIED BY 'maxpwd';
-GRANT ALL ON *.* TO 'maxuser'@'127.0.0.1' WITH GRANT OPTION;
+GRANT ALL ON *.* TO 'maxuser'@'172.16.16.65' WITH GRANT OPTION;
 GRANT ALL ON *.* TO 'maxuser'@'%' WITH GRANT OPTION;
 
 CREATE DATABASE test;
