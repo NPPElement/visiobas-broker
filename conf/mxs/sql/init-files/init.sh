1f308d337e8769a602c24fe959c6f23a5b5d678d
John <e.k@mail.ru>
2021-01-23 21:59:36 +0300

    Update init.sh

--- conf/mxs/sql/init-files/init.sh
+++ conf/mxs/sql/init-files/init.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 # Разворачиваем базу данных
-mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD < /opt/user.sql
-
-# Разворачиваем базу данных
 mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD < /opt/init.sql
 
 # Заполняем начальные данные
