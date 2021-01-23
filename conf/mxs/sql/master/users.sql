f5c8b211441c0911a2636b9e8726f8de1d393a1d
John <e.k@mail.ru>
2021-01-23 18:43:02 +0300

    Update users.sql

--- conf/mxs/sql/master/users.sql
+++ conf/mxs/sql/master/users.sql
@@ -5,6 +5,10 @@ SET GLOBAL gtid_strict_mode=ON;
 
 -- создаем пользователя для доступа к базе данных с visiodesk 
 -- и разрешаем доступ с любого хоста в сети 172.16.16.0/24
-CREATE USER 'visiobas'@'172.16.16.%' IDENTIFIED BY 'locpa$$';
-GRANT ALL PRIVILEGES ON *.* TO 'visiobas'@'172.16.16.%';
-GRANT ALL ON *.* TO 'visiobas'@'172.16.16.%' WITH GRANT OPTION;
+-- CREATE USER 'visiobas'@'172.16.16.%' IDENTIFIED BY 'locpa$$';
+-- GRANT ALL PRIVILEGES ON *.* TO 'visiobas'@'172.16.16.%';
+-- GRANT ALL ON *.* TO 'visiobas'@'172.16.16.%' WITH GRANT OPTION;
+
+CREATE USER 'visiobas'@'%' IDENTIFIED BY 'locpa$$';
+GRANT ALL PRIVILEGES ON *.* TO 'visiobas'@'%';
+GRANT ALL ON *.* TO 'visiobas'@'%' WITH GRANT OPTION;
