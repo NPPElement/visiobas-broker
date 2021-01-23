cff9f52a05c198179cb1c8f9d5262c67d6407065
John <e.k@mail.ru>
2021-01-24 01:49:26 +0300

    sql

--- conf/mxs/sql/master/users.sql
+++ conf/mxs/sql/master/users.sql
@@ -17,20 +17,15 @@ SET GLOBAL gtid_strict_mode=ON;
 -- создаем пользователя для доступа к базе данных с visiodesk 
 -- и разрешаем доступ с любого хоста в сети 172.16.16.0/24
 
-CREATE USER 'visiobas'@'172.16.16.60' IDENTIFIED BY 'locpa$$';
-GRANT ALL ON *.* TO 'visiobas'@'172.16.16.60' WITH GRANT OPTION;
+CREATE USER 'visiobas'@'%' IDENTIFIED BY 'locpa$$';
+GRANT ALL ON *.* TO 'visiobas'@'%' WITH GRANT OPTION;
 
-CREATE USER 'backup'@'172.16.16.42' IDENTIFIED BY 'backupa$$';
-GRANT SELECT ON *.* TO 'backup'@'172.16.16.42' WITH GRANT OPTION;
+CREATE USER 'monitor'@'%' IDENTIFIED BY 'monitorpa$$';
+GRANT ALL ON *.* TO 'monitor'@'%' WITH GRANT OPTION;
+
+CREATE USER 'backup'@'172.16.16.%' IDENTIFIED BY 'backupa$$';
+GRANT SELECT ON *.* TO 'backup'@'172.16.16.%' WITH GRANT OPTION;
 
 CREATE USER 'master'@'172.16.16.6%' IDENTIFIED BY 'masterpa$$';
 GRANT ALL ON *.* TO 'master'@'172.16.16.6%' WITH GRANT OPTION;
 
-CREATE USER 'monitor'@'172.16.16.60' IDENTIFIED BY 'monitorpa$$';
-GRANT ALL ON *.* TO 'monitor'@'172.16.16.60' WITH GRANT OPTION;
-
-CREATE USER 'read'@'172.16.16.60' IDENTIFIED BY 'readpa$$';
-GRANT ALL ON *.* TO 'read'@'172.16.16.60' WITH GRANT OPTION;
-
-CREATE USER 'write'@'172.16.16.60' IDENTIFIED BY 'writepa$$';
-GRANT ALL ON *.* TO 'write'@'172.16.16.60' WITH GRANT OPTION;
