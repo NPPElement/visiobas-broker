dd0e1782e960231a4fcb9b377c108691d7a6f2d0
John <e.k@mail.ru>
2021-01-24 02:23:16 +0300

    Update users.sql

--- conf/mxs/sql/master/users.sql
+++ conf/mxs/sql/master/users.sql
@@ -17,19 +17,20 @@ SET GLOBAL gtid_strict_mode=ON;
 -- создаем пользователя для доступа к базе данных с visiodesk 
 -- и разрешаем доступ с любого хоста в сети 172.16.16.0/24
 
-CREATE USER 'maxuser'@'127.0.0.1' IDENTIFIED BY 'maxpwd';
 CREATE USER 'maxuser'@'%' IDENTIFIED BY 'maxpwd';
-CREATE USER 'visiobas'@'%' IDENTIFIED BY 'locpa$$';
-GRANT ALL ON *.* TO 'maxuser'@'127.0.0.1' WITH GRANT OPTION;
+CREATE USER 'maxuser'@'127.0.0.1' IDENTIFIED BY 'maxpwd';
 GRANT ALL ON *.* TO 'maxuser'@'%' WITH GRANT OPTION;
+GRANT ALL ON *.* TO 'maxuser'@'127.0.0.1' WITH GRANT OPTION;
+
+CREATE USER 'visiobas'@'172.16.16.50' IDENTIFIED BY 'locpa$$';
+CREATE USER 'visiobas'@'%' IDENTIFIED BY 'locpa$$';
+GRANT ALL ON *.* TO 'visiobas'@'172.16.16.50' WITH GRANT OPTION;
 GRANT ALL ON *.* TO 'visiobas'@'%' WITH GRANT OPTION;
 
-CREATE USER 'monitor'@'%' IDENTIFIED BY 'monitorpa$$';
-GRANT ALL ON *.* TO 'monitor'@'%' WITH GRANT OPTION;
-
-CREATE USER 'backup'@'%172.16.16.%' IDENTIFIED BY 'backupa$$';
-GRANT SELECT ON *.* TO 'backup'@'%172.16.16.%' WITH GRANT OPTION;
+CREATE USER 'backup'@'172.16.16.42' IDENTIFIED BY 'backupa$$';
+CREATE USER 'backup'@'%' IDENTIFIED BY 'backupa$$';
+GRANT ALL ON *.* TO 'backup'@'172.16.16.42' WITH GRANT OPTION;
+GRANT ALL ON *.* TO 'backup'@'%' WITH GRANT OPTION;
 
 CREATE USER 'master'@'172.16.16.6%' IDENTIFIED BY 'masterpa$$';
 GRANT ALL ON *.* TO 'master'@'172.16.16.6%' WITH GRANT OPTION;
-
