b49712f07ee16b1455e5e503b33212aff826291e
NPPElement <you@example.com>
2021-02-17 05:28:24 +0000

    php

--- conf/php-8/projects/index.php
+++ conf/php-8/projects/index.php
@@ -1,3 +1,6 @@
 <?php
 
-phpinfo();
+echo "Here2";
+
+file_put_contents("tets.json", json_encode(["a"=>1,"b"=>3]));
+
---
+++ conf/php-8/projects/lib/mymqtt.php
@@ -0,0 +1,20 @@
+<?php
+
+include  __DIR__."/MQTTClient.php";
+
+global $mymqttClient;
+$mymqttClient = new MQTTClient("visiodesk.net", 1883);
+$mymqttClient->setAuthentication("user", "user");
+$ok = $mymqttClient->sendConnect("phpclient".rand(100,999));
+if(!$ok) die("Нет соединения с брокером");
+
+function publishMqttValue($reference, $devId, $objId, $objType, $value, $status=0) {
+    echo "publishMqttValue($reference, $devId, $objId, $objType, $value, $status)\n";
+    return;
+    global $mymqttClient;
+    $reference = str_replace(":", "/", $reference);
+    $reference = str_replace(".", "/", $reference);
+    $mymqttClient->sendPublish($reference, "$devId $objId $objType $value $status", 0, 1);
+}
+
+
