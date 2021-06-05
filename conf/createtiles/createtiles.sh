0f82e4a8c14858523747b3fc818d80eeb2d4eec1
John <e.k@mail.ru>
2021-06-05 23:34:16 +0300

    Update createtiles.sh

--- conf/createtiles/createtiles.sh
+++ conf/createtiles/createtiles.sh
@@ -10,8 +10,15 @@ test ! -f gdal2tiles.py \
   > gdal2tiles.py \
   && echo "'python-gdal' library required - please install sudo apt install python-gdal"
 
+if [ $# -eq 0 ]
+then
+  echo "Нет файла в параметрах"
+  exit 1
+fi
+src_file=$1
+
 # process ...
 export GDAL_ALLOW_LARGE_LIBJPEG_MEM_ALLOC=1
-python ./gdal2tiles.py -l -p raster -z 0-4 -w none WP--20-208.png tiles
+python ./gdal2tiles.py -l -p raster -z 0-4 -w none $src_file $src_file
 
 echo ' '
