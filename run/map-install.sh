62e5adc4f5e2c7e09188425ba17e7550e265232d
Nppelement <e.k@mail.ru>
2021-09-06 08:41:00 +0300

    Update map-install.sh

--- run/map-install.sh
+++ run/map-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # 
- 
-time docker run -v /opt/zambia-latest.osm.pbf:/data.osm.pbf -v /opt/services/home/openstreetmap-data/:/var/lib/postgresql/12/main overv/openstreetmap-tile-server:1.3.10 import
+cd /opt/
+wget https://planet.maps.mail.ru/pbf/planet-latest.osm.pbf 
+time docker run -v /opt/planet-latest.osm.pbf:/data.osm.pbf -v /opt/services/home/openstreetmap-data/:/var/lib/postgresql/12/main overv/openstreetmap-tile-server:1.3.10 import
